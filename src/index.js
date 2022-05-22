5f0b7f611f655664b624f6b83c3479325d94a495
AbolnikovAnton <bakunin.abolnikov@gmail.com>
2022-05-22 21:52:42 +0300

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import {Provider} from 'react-redux'
 import {configureStore} from "@reduxjs/toolkit";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const store = configureStore({
     reducer: {},
